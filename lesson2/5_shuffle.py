8e836f0416367d3466f5193259ac93ff80870f15
GennadiySerikov <serikov.gen@yandex.ru>
2022-09-29 18:57:51 +0300

    feat: update shuffle

--- lesson2/5_shuffle.py
+++ lesson2/5_shuffle.py
@@ -5,10 +5,6 @@ numbers = list(range(n))
 print(numbers)
 
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> fd884ed8929150501bb50e04cf050b5df388de78
 def random_int(seed: int) -> int:
     return int(random.random() * seed)
 
@@ -23,32 +19,7 @@ def shuffle(mass: list) -> enumerate:
             _access_indexes.remove(index)
 
     return _result
-<<<<<<< HEAD
-=======
-=======
-def random_int(seed: int) -> int: int(random.random() * seed)
-
-
-def shuffle(mass: list) -> enumerate:
-    result = []
-    access_indexes = list(range(n))
-    while len(access_indexes) > 0:
-        index = int(random.random() * 10)
-        if index in access_indexes:
-            result.append(numbers[index])
-            access_indexes.remove(index)
-
-    return result
->>>>>>> 8b769e35c83271ee2317c9241536ac21c1187fa6
->>>>>>> fd884ed8929150501bb50e04cf050b5df388de78
 
 
 result = shuffle(numbers)
 print(result)
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-=======
->>>>>>> 8b769e35c83271ee2317c9241536ac21c1187fa6
->>>>>>> fd884ed8929150501bb50e04cf050b5df388de78
