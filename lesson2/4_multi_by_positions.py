1874c5b5835002d49b36f96b044219d57dfe897c
GennadiySerikov <serikov.gen@yandex.ru>
2022-09-30 22:04:57 +0300

    fix: check for size less than positions

--- lesson2/4_multi_by_positions.py
+++ lesson2/4_multi_by_positions.py
@@ -3,6 +3,9 @@ pos2 = int(input("Position two: ")) - 1
 
 n = int(input("Number of elements: "))
 
+if n < pos1 < pos2:
+    raise Exception("array size less than positions")
+
 numbers = list(range(-n, n + 1))
 
 print(numbers)
