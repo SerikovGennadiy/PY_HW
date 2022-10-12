418d7b6c0a3bd42194fa92ec9f283593fecca37a
GennadiySerikov <serikov.gen@yandex.ru>
2022-10-12 23:16:35 +0300

    feat: full_names

--- lesson6/4_full_names.py
+++ lesson6/4_full_names.py
@@ -2,16 +2,20 @@ import itertools
 
 # full_names_input = input("insert full names e.g: Иван Сергеев, Инна Серова, Петр Алексеев : ")
 full_names_input = "Иван Сергеев, Инна Серова, Петр Алексеев, Илья Иванов, Анна Савельева, Юнона Ветрякова, Борис Аркадьев, Антон Серов, Павел Анисимов"
-full_names = sorted(full_names_input.split(", "),
-                    key=lambda fn: fn.split()[1][0])
-# print(full_names)
 
-full_names_by_last_name = dict([(n, list(k)) for n, k in
-                                itertools.groupby(full_names, key=lambda n: n.split()[1][0])])
-# print(full_names_by_last_name)
 
-for n in full_names_by_last_name:
-    full_names_by_last_name[n] = dict([(x, list(y)) for x, y in
-                                  itertools.groupby(sorted(full_names_by_last_name[n]), key=lambda x: x[0])])
+def get_order_face_book(_full_names_input):
+    full_names = sorted(_full_names_input.split(", "),
+                        key=lambda fn: fn.split()[1][0])
 
-print(full_names_by_last_name)
+    full_names_by_last_name = dict([(n, list(k)) for n, k in
+                                    itertools.groupby(full_names, key=lambda n: n.split()[1][0])])
+    for n in full_names_by_last_name:
+        full_names_by_last_name[n] = dict([(x, list(y)) for x, y in
+                                           itertools.groupby(sorted(full_names_by_last_name[n]), key=lambda x: x[0])])
+
+    return full_names_by_last_name
+
+
+print(get_order_face_book(full_names_input))
+
