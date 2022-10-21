a8a8c5d861d8c6905b1c57f83784c531c2569861
GennadiySerikov <serikov.gen@yandex.ru>
2022-10-21 22:28:46 +0300

    feat: abc

--- lesson7/calculator/controller.py
+++ lesson7/calculator/controller.py
@@ -17,26 +17,39 @@ def start():
         if q.startswith('y'):
             log.show_log()
             return
-        _presetup_calc()
-        operands = _get_operands()
-        result = make_calculation(operation_type, operands)
-        log.write_log(operation_type, operands, result)
-        ui.show_result(result)
+        try:
+            _presetup_calc()
+            operands = _get_operands()
+            result = make_calculation(operation_type, operands)
+            log.write_log(operation_type, operands, result)
+            show_result(operands, result)
+        except Exception as exp:
+            print(str(exp))
+
+
+def show_result(operands, result):
+    ui.show_result(f'{operation_type.name} of {operands[0]} and {operands[1]} is equal {result}')
 
 
 def _get_operands() -> (object, object):
     match number_type:
         case NumType.Complex:
-            real_first, image_first = \
-                float(input("insert first real part: ")), \
-                float(input("insert first image part: "))
-            real_second, image_second = \
-                float(input("insert second real part: ")), \
-                float(input("insert second image part: "))
+            try:
+                real_first, image_first = \
+                    float(input("insert first real part: ")), \
+                    float(input("insert first image part: "))
+                real_second, image_second = \
+                    float(input("insert second real part: ")), \
+                    float(input("insert second image part: "))
+            except BaseException:
+                raise Exception("error during get complex operands!")
             return complex(real_first, image_first), complex(real_second, image_second)
         case NumType.Real:
-            first = float(input("insert first number: "))
-            second = float(input("insert second number: "))
+            try:
+                first = float(input("insert first number: "))
+                second = float(input("insert second number: "))
+            except BaseException:
+                raise Exception("error during get real operands!")
             return first, second
 
 
@@ -47,9 +60,15 @@ def _presetup_calc():
 
 def _choice_num_type():
     global number_type
-    number_type = NumType(int(input('Insert number type: ')))
+    try:
+        number_type = NumType(int(input('Insert number type: ')))
+    except BaseException:
+        raise Exception('error during selecting number type!')
 
 
 def _choice_operation():
     global operation_type
-    operation_type = Operation(int(input('Insert operation type: ')))
+    try:
+        operation_type = Operation(int(input('Insert operation type: ')))
+    except BaseException:
+        raise Exception('error during selecting operation type!')
